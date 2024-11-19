07dfde90046b26f6b85b1f22e1777679b862b6f0
LL965 <2487817612@qq.com>
2024-11-19 13:49:19 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
 //请在下方输入你的代码：
+//正三角
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    for (i = 1; i <= 9; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d*%d=%2d ", j, i, i* j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//倒三角
+#include<stdio.h>
+int main(){
+    for (int i = 1; i <= 9; i++){
+        for (int j = 1; j <= 9; j++) {
+            if (i < j)
+            {
+                printf("%d*%d=%2d", i, j, i * j);
+            }
+            else
+            {
+                printf("\n");
+            }
+        }
+    }
+    return 0;
+}
